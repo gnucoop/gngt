be082b2e5bd8d1f7313b6bdf36b5ddc35951e569
trik <marco.marche@gnucoop.com>
2018-11-24 18:23:56 +0100

    build: removed payload and coverage gulp tasks

--- tools/dashboard/src/environments/firebase.ts
+++
@@ -1,7 +0,0 @@
-/** Shared Firebase configuration that will be used in every environment. */
-export const firebaseConfig = {
-  apiKey: 'AIzaSyCbDnV4udMgMciQ3Sva88Ji1udOIPltDAA',
-  authDomain: 'gngt-board.firebaseapp.com',
-  databaseURL: 'https://gngt-board.firebaseio.com',
-  projectId: 'gngt-board'
-};
