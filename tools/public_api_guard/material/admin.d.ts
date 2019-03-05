a3fd93b22d0dfb9e13dcc9cf06ccb93d86339aae
trik <marco.marche@gnucoop.com>
2019-03-05 15:34:27 +0100

    build: fix material admin api definition

--- tools/public_api_guard/material/admin.d.ts
+++ tools/public_api_guard/material/admin.d.ts
@@ -2,8 +2,12 @@ export declare class AdminEditComponent<T extends Model, S extends fromModel.Sta
     constructor(cdr: ChangeDetectorRef, fb: FormBuilder, router: Router);
 }
 
-export declare class AdminListComponent<T extends Model, S extends fromModel.State<T>, A1 extends ModelActions.ModelGetAction, A2 extends ModelActions.ModelListAction, A3 extends ModelActions.ModelCreateAction<T>, A4 extends ModelActions.ModelUpdateAction<T>, A5 extends ModelActions.ModelPatchAction<T>, A6 extends ModelActions.ModelDeleteAction<T>, A7 extends ModelActions.ModelDeleteAllAction<T>, MS extends ModelService<T, S, A1, A2, A3, A4, A5, A6, A7>> extends BaseAdminListComponent<T, S, A1, A2, A3, A4, A5, A6, A7, MS> implements OnDestroy, OnInit {
+export declare class AdminListComponent<T extends Model, S extends fromModel.State<T>, A1 extends ModelActions.ModelGetAction, A2 extends ModelActions.ModelListAction, A3 extends ModelActions.ModelCreateAction<T>, A4 extends ModelActions.ModelUpdateAction<T>, A5 extends ModelActions.ModelPatchAction<T>, A6 extends ModelActions.ModelDeleteAction<T>, A7 extends ModelActions.ModelDeleteAllAction<T>, MS extends ModelService<T, S, A1, A2, A3, A4, A5, A6, A7>> extends BaseAdminListComponent<T, S, A1, A2, A3, A4, A5, A6, A7, MS> implements AfterContentInit, OnDestroy, OnInit {
     actionSel: MatSelect;
+    cellTemplates: QueryList<AdminListCellDirective>;
+    readonly cellTemplatesMap: {
+        [column: string]: TemplateRef<any>;
+    };
     dataSource: ModelDataSource<T, S, A1, A2, A3, A4, A5, A6, A7, MS>;
     paginatorCmp: MatPaginator;
     readonly selection: SelectionModel<T>;
@@ -12,6 +16,7 @@ export declare class AdminListComponent<T extends Model, S extends fromModel.Sta
     clearSelection(): void;
     getItems(): T[];
     getSelection(): T[];
+    ngAfterContentInit(): void;
     ngOnInit(): void;
     refreshList(): void;
     selectAll(): void;
