d13e030afcf90a16d15364b90fd72e924866a223
trik <marco.marche@gnucoop.com>
2020-03-19 13:41:27 +0100

    build: update to angular v9.0.6

--- tools/release/release-output/output-validations.ts
+++ tools/release/release-output/output-validations.ts
@@ -8,27 +8,27 @@ const inlineStylesSourcemapRegex = /styles: ?\[["'].*sourceMappingURL=.*["']/;
 /** RegExp that matches Angular component metadata properties that refer to external resources. */
 const externalReferencesRegex = /(templateUrl|styleUrls): *["'[]/;
 
-/**
- * List of fields which are mandatory in entry-point "package.json" files and refer
- * to files in the release output.
- */
-const packageJsonPathFields =
-    ['main', 'module', 'typings', 'es2015', 'fesm5', 'fesm2015', 'esm5', 'esm2015'];
+/** RegExp that matches common Bazel manifest paths in this workspace */
+const bazelManifestPath = /(gngt|external)\//;
 
 /**
- * Checks the specified release bundle and ensures that it does not contain
- * any external resource URLs.
+ * Checks the specified JavaScript file and ensures that it does not
+ * contain any external resource URLs, or Bazel manifest paths.
  */
-export function checkReleaseBundle(bundlePath: string): string[] {
-  const bundleContent = readFileSync(bundlePath, 'utf8');
+export function checkJavaScriptOutput(filePath: string): string[] {
+  const fileContent = readFileSync(filePath, 'utf8');
   const failures: string[] = [];
 
-  if (inlineStylesSourcemapRegex.exec(bundleContent) !== null) {
+  if (inlineStylesSourcemapRegex.exec(fileContent) !== null) {
     failures.push('Found sourcemap references in component styles.');
   }
 
-  if (externalReferencesRegex.exec(bundleContent) !== null) {
+  if (externalReferencesRegex.exec(fileContent) !== null) {
     failures.push('Found external component resource references');
+  }
+
+  if (bazelManifestPath.exec(fileContent) !== null) {
+    failures.push('Found Bazel manifest path in output.');
   }
 
   return failures;
