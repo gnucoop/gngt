377fd4f7450a8625dfdb75e133d8ed3fd508f948
trik <marco.marche@gnucoop.com>
2020-05-15 10:36:33 +0200

    build: calcultate CI cache key in a reproducible way

--- scripts/jenkins/cache-key.sh
+++ scripts/jenkins/cache-key.sh
@@ -2,4 +2,10 @@
 
 CACHE_KEY_FILES=".bazelversion tools/postinstall/apply-patches.js WORKSPACE yarn.lock"
 
-echo `tar cf - ${CACHE_KEY_FILES} | sha256sum | awk '{print $1}'`
+TOTAL_SUM=""
+for CACHE_KEY_FILE in "${CACHE_KEY_FILES[@]}"; do
+  SUM=`sha256sum -b ${CACHE_KEY_FILE} | awk '{print $1}'`
+  TOTAL_SUM="${TOTAL_SUM}${SUM}"
+done
+
+echo `echo ${TOTAL_SUM} | sha256sum -b - | awk '{print $1}'`
