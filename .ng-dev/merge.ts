3b1d07cd42284392296a6d85c598e02e58623d74
trik <marco.marche@gnucoop.com>
2020-06-10 12:09:00 +0200

    build: switch to dev-infra configuration directory

--- .ng-dev/merge.ts
+++ .ng-dev/merge.ts
@@ -1,22 +1,12 @@
 import {MergeConfig} from '@angular/dev-infra-private/pr/merge/config';
 import {determineMergeBranches} from '@angular/dev-infra-private/pr/merge/determine-merge-branches';
-import {GithubConfig} from '@angular/dev-infra-private/utils/config';
-
-/**
- * Github configuration for the ng-dev command. This repository is
- * uses as remote for the merge script.
- */
-const github: GithubConfig = {
-  owner: 'gnucoop',
-  name: 'gngt'
-};
 
 /**
  * Configuration for the merge tool in `ng-dev`. This sets up the labels which
  * are respected by the merge script (e.g. the target labels).
  */
-const merge = (): MergeConfig => {
-  const currentVersion = require('./package.json').version;
+export const merge = (): MergeConfig => {
+  const currentVersion = require('../package.json').version;
   // We use the `@gngt/core` as source of truth for the latest published version in NPM.
   // Any package from the monorepo could technically work and result in the same version.
   const {minor, patch} = determineMergeBranches(currentVersion, '@gngt/core');
