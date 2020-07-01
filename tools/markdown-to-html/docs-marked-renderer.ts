11a8c24671fd968f554d9ea5d0fa07efa9129060
trik <marco.marche@gnucoop.com>
2020-07-01 16:27:48 +0200

    docs: prevent 'undefined' from appearing in embedded example filenames
    In cases when the extracted `file` or `region` from an embedded example comment was undefined, leading to the filename included "undefined" in the string. This changes the markdown processor to emit an empty string instead.

--- tools/markdown-to-html/docs-marked-renderer.ts
+++ tools/markdown-to-html/docs-marked-renderer.ts
@@ -77,19 +77,17 @@ export class DocsMarkdownRenderer extends Renderer {
    *  `<div material-docs-example="name"></div>`
    */
   html(html: string) {
-    html = html.replace(
-        exampleCommentRegex,
-        (_match: string,
-         content: string) => {  // using [\s\S]* because .* does not match line breaks
-          if (content.match(/\{[\s\S]*\}/g)) {
-            const {example, file, region} = JSON.parse(content);
-            return `<div material-docs-example="${example}"
-                           file="${file}"
-                           region="${region}"></div>`;
-          } else {
-            return `<div material-docs-example="${content}"></div>`;
-          }
-        });
+    html = html.replace(exampleCommentRegex, (_match: string, content: string) => {
+      // using [\s\S]* because .* does not match line breaks
+      if (content.match(/\{[\s\S]*\}/g)) {
+        const {example, file, region} = JSON.parse(content);
+        return `<div material-docs-example="${example}"
+                             ${file ? `file="${file}"` : ''}
+                             ${region ? `region="${region}"` : ''}></div>`;
+      } else {
+        return `<div material-docs-example="${content}"></div>`;
+      }
+    });
 
     return super.html(html);
   }
