b6c3d0ab8272724cc2c991e29fa48e30e79ce8f3
Marco Marche <marco.marche@gnucoop.com>
2021-03-15 10:57:25 +0100

    build: update to gulp 4
    We still depend on Gulp for a couple of CI tasks, but we've been stuck on version 3.8 for
    a while which doesn't support Node 12 and might break in other ways in the future.
    
    These changes update us to version 4.0.2, account for the breaking changes and remove
    some dependencies that have been replaced by functions in Gulp.

--- tools/gulp/gulpfile.ts
+++ tools/gulp/gulpfile.ts
@@ -5,10 +5,11 @@ import {
   materialPackage,
 } from './packages';
 
-import './tasks/ci';
-import './tasks/clean';
-import './tasks/unit-test';
-
+// Build tasks have to be imported first, because the other tasks depend on them.
 createPackageBuildTasks(corePackage);
 createPackageBuildTasks(ionicPackage);
 createPackageBuildTasks(materialPackage);
+
+import './tasks/clean';
+import './tasks/unit-test';
+import './tasks/ci';
