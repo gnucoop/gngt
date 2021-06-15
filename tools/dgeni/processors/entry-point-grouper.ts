fc705168ad2796d89140ccd07bf2d2d2be05a917
Marco Marche <marco.marche@gnucoop.com>
2021-06-15 18:31:25 +0200

    docs: fix inherited docs-private members showing up
    We recently landed a couple of improvements in the Dgeni pipeline, so
    that descriptions of inherited members show up. To achieve this, we
    applied a workaround that basically meant we are parsing the API doc's`
    string cotntent manually to a JSDoc description (this also involved
    manually stripping any JSDoc annotations).
    
    This commit replaces the workaround with a solution that allows us
    to still run necessary default Dgeni processors on the inherited API
    docs, such as the offical `jsdoc` processors. These solve the problem
    of manually extracting the JSDOc description, and also fix more
    down-the-line problems where e.g. `@docs-private` is not respected
    for inherited members. It also fixes cases where the JSDOc annotations
    are still showing up in the docs.

--- tools/dgeni/processors/entry-point-grouper.ts
+++ tools/dgeni/processors/entry-point-grouper.ts
@@ -90,6 +90,7 @@ export class EntryPointDoc {
 export class EntryPointGrouper implements Processor {
   name = 'entryPointGrouper';
   $runBefore = ['docs-processed'];
+  $runAfter = ['docs-private-filter'];
   entryPoints: string[] = [];
 
   $process(docs: DocCollection) {
