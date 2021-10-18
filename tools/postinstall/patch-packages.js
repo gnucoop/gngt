342850bd1ec08adffd202c4f550dd1bbf5d79531
trik <marco.marche@gnucoop.com>
2021-10-18 09:18:30 +0200

    feat(multiple): add support for TypeScript 4.4
    Updates to the latest version of Angular and TypeScript, and resolves the various build errors.

--- tools/postinstall/patch-packages.js
+++ tools/postinstall/patch-packages.js
@@ -15,3 +15,8 @@ packageJsonPath = path.posix.join('node_modules', 'pouchdb', 'package.json');
 packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
 packageJson.browser = './dist/pouchdb.js';
 fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson));
+
+const ioniconsFile = 'node_modules/ionicons/dist/types/components.d.ts';
+let ionicons = fs.readFileSync(ioniconsFile, 'utf8');
+ionicons = ionicons.replace(/"aria(Hidden|Label)"\?:/gm, '"aria$1":');
+fs.writeFileSync(ioniconsFile, ionicons);
