0ea0df5e86ff580514a6ac7f4ccfb19441bf22ea
trik <marco.marche@gnucoop.com>
2021-10-18 09:39:36 +0200

    build: remove view engine workarounds for old APF output
    Removes all View Engine workarounds that we previously needed to generate the APF package output with View Engine.

--- tools/postinstall/patch-packages.js
+++ tools/postinstall/patch-packages.js
@@ -1,20 +1,15 @@
 const fs = require('fs');
-const path = require('path');
 
-const packages = ['@gic', '@ionic'];
-
-packages.forEach(package => {
-  const packageJsonPath = path.posix.join('node_modules', package, 'core', 'loader', 'package.json');
-  const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
-  packageJson.name = `${package}/core/loader`;
-  packageJson.browser = './index.cjs.js';
-  fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson));
-});
-
-packageJsonPath = path.posix.join('node_modules', 'pouchdb', 'package.json');
-packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
-packageJson.browser = './dist/pouchdb.js';
-fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson));
+const ngDevFile = 'node_modules/@angular/dev-infra-private/ng-dev/cli-bundle.js';
+let ngDev = fs.readFileSync(ngDevFile, 'utf8');
+ngDev = ngDev.replace(
+`    function getReleaseTagForVersion(version) {
+      return version.format();
+    }`,
+`    function getReleaseTagForVersion(version) {
+      return 'v' + version.format();
+    }`);
+fs.writeFileSync(ngDevFile, ngDev);
 
 const ioniconsFile = 'node_modules/ionicons/dist/types/components.d.ts';
 let ionicons = fs.readFileSync(ioniconsFile, 'utf8');
