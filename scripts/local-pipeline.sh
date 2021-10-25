b84c2f357c4add913c4a65427900198f44262560
trik <marco.marche@gnucoop.com>
2021-10-25 11:02:08 +0200

    ci: change steps order

--- scripts/local-pipeline.sh
+++ scripts/local-pipeline.sh
@@ -7,15 +7,6 @@ BAZEL_BINARY="./node_modules/.bin/bazel"
 echo "Setup"
 yarn install --non-interactive
 
-echo "Unit tests - View Engine"
-"${BAZEL_BINARY}" test --build_tag_filters=-docs-package,-e2e --test_tag_filters=-e2e --config=view-engine --build_tests_only -- src/...
-
-echo "Build - View Engine"
-"${BAZEL_BINARY}" build --build_tag_filters=-docs-package,-release-package --config=view-engine -- src/...
-
-echo "Test local browsers"
-"${BAZEL_BINARY}" test --build_tag_filters=-e2e --test_tag_filters=-e2e --build_tests_only -- src/...
-
 echo "Lint"
 "${BAZEL_BINARY}" build //:rollup_globals
 yarn check-rollup-globals $("${BAZEL_BINARY}" info bazel-bin)/rollup_globals.json
@@ -24,14 +15,29 @@ yarn check-entry-point-setup $("${BAZEL_BINARY}" info bazel-bin)/entry_points_ma
 yarn -s lint
 yarn -s ts-circular-deps:check
 
+echo "Build - View Engine"
+"${BAZEL_BINARY}" build --build_tag_filters=-docs-package,-release-package --config=view-engine -- src/...
+
+echo "Unit tests - View Engine"
+"${BAZEL_BINARY}" test --build_tag_filters=-docs-package,-e2e --test_tag_filters=-e2e --config=view-engine --build_tests_only -- src/...
+
 echo "Integration tests - View Engine"
 yarn integration-tests:view-engine
 
-echo "Integration tests - Partial Ivy"
-yarn integration-tests:partial-ivy
+echo "Build"
+"${BAZEL_BINARY}" build --build_tag_filters=-docs-package,-release-package -- src/...
+
+echo "API golden checks"
+"${BAZEL_BINARY}" test tools/public_api_guard/...
+
+echo "Test local browsers"
+"${BAZEL_BINARY}" test --build_tag_filters=-e2e --test_tag_filters=-e2e --build_tests_only -- src/...
 
 echo "E2E tests"
 yarn e2e --flaky_test_attempts=2
+
+echo "Integration tests - Partial Ivy"
+yarn integration-tests:partial-ivy
 
 echo "Build release packages"
 yarn build-and-check-release-output
@@ -45,9 +51,3 @@ mv node_modules/__ngcc_entry_points__.json node_modules/__ngcc_entry_points__.js
 yarn ngcc --error-on-failed-entry-point --no-tsconfig
 mv node_modules/__ngcc_entry_points__.json.back node_modules/__ngcc_entry_points__.json
 rm -Rf node_modules/@gngt
-
-echo "Build"
-"${BAZEL_BINARY}" build --build_tag_filters=-docs-package,-release-package -- src/...
-
-echo "API golden checks"
-"${BAZEL_BINARY}" test tools/public_api_guard/...
